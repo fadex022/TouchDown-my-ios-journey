fcde5eb8b3b541ad33db8250d0e2a289edc886d0
Fadel Bidiga <bidigafadel@gmail.com>
2022-05-01 11:56:16 +0000

    Initial Commit

---
+++ TouchDown/TouchDownApp.swift
@@ -0,0 +1,17 @@
+//
+//  TouchDownApp.swift
+//  TouchDown
+//
+//  Created by Gauss on 01/05/2022.
+//
+
+import SwiftUI
+
+@main
+struct TouchDownApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
