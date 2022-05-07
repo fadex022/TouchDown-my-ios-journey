a54f949c76e28ad6e6e1c24aa5f90a8d1396ea21
Fadel Bidiga <bidigafadel@gmail.com>
2022-05-07 12:19:28 +0000

    Create a model for player.json file and a decoder for the json files

---
+++ TouchDown/Models/PlayerModel.swift
@@ -0,0 +1,13 @@
+//
+//  PlayerModel.swift
+//  TouchDown
+//
+//  Created by Gauss on 07/05/2022.
+//
+
+import Foundation
+
+struct Player: Codable, Identifiable {
+    let id: Int
+    let image: String
+}
