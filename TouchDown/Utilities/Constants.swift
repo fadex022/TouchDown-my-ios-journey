292c1168ea9a076aca58ec055a15522326927ae2
Fadel Bidiga <bidigafadel@gmail.com>
2022-05-07 11:22:57 +0000

    setup the project and create the FooterView

---
+++ TouchDown/Utilities/Constants.swift
@@ -0,0 +1,21 @@
+//
+//  Constants.swift
+//  TouchDown
+//
+//  Created by Gauss on 07/05/2022.
+//
+
+import SwiftUI
+
+// DATA
+// COLOR
+let colorBackground: Color = Color("ColorBackground")
+let colorGray: Color = Color(UIColor.systemGray4)
+
+// LAYOUT
+// UX
+// API
+// IMAGE
+// FONT
+// STRING
+// MISC
---
+++ TouchDown/Views/Home/FooterView.swift
@@ -0,0 +1,41 @@
+//
+//  FooterView.swift
+//  TouchDown
+//
+//  Created by Gauss on 07/05/2022.
+//
+
+import SwiftUI
+
+struct FooterView: View {
+    var body: some View {
+        VStack(alignment: .center, spacing: 10) {
+            Text("We offer the most cutting edge, comfortable, lightweight and durable football helmets in the market at affordable prices")
+                .foregroundColor(.gray)
+                .multilineTextAlignment(.center)
+                .layoutPriority(2)
+            
+            Image("logo-lineal")
+                .renderingMode(.template)
+                .foregroundColor(.gray)
+                .layoutPriority(0)
+            
+            Text("Copyright © Fadel Bidiga \nAll right reserved")
+                .font(.footnote)
+                .foregroundColor(.gray)
+                .fontWeight(.bold)
+                .multilineTextAlignment(.center)
+                .layoutPriority(1)
+            
+        }//: VSTACK
+        .padding()
+    }
+}
+
+struct FooterView_Previews: PreviewProvider {
+    static var previews: some View {
+        FooterView()
+            .previewLayout(.sizeThatFits)
+            .background(colorBackground)
+    }
+}
